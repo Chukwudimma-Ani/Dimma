db84cf21d2560787201834471e3c64e4f641da02
Chukwudimma Ani <152604214+Chukwudimma-Ani@users.noreply.github.com>
2024-05-28 16:24:29 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
           <h3>Designed by Chukwudimma Ani</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {name}{ year} CA.</h3>
+          <h3>Copyright © {name}, { year} CA.</h3>
           <h3>Chiejina Akajona Ubosi</h3>
         </Col>
         <Col md="4" className="footer-body">
